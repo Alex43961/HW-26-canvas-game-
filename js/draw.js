3af7f49214b52c7216bfca3bd2ae22dce25ef124
Александр Семенченко <alex439@MacBook-Pro-Aleksandr.local>
2023-05-07 03:54:52 +0300

    draw finished

--- js/draw.js
+++ js/draw.js
@@ -1,8 +1,35 @@
+'use strict'
+let imgS = [];
+
 export function draw() {
 	const imgList = ["./images/small-background.png", "./images/my spaceship.png", "./images/my rocket.png", "./images/enemy's spaceship.png", "./enemy's rocket.png"];
+
 	imagesInit();
 
 	const canvas = document.getElementById("canvas");
 	const ctx = canvas.getContext('2d');
-	ctx.drawImages()
+	ctx.drawImage(imgS[0], 0, 0);
+	ctx.drawImage(imgS[1], userPositionX, userPositionY);//перемення задана в main.js
+
+	for (let i = 0; i < rockets.length; i++) {
+		ctx.drawImage(imgS[2], rockets[i].x, rockets[i].y);
+	}
+	for (let i = 0; i < enemies.length; i++) {
+		ctx.drawImage(imgS[3], enemies[i].x, enemies[i].y);
+	}
+	for (let i = 0; i < enemyRockets.length; i++) {
+		ctx.drawImage(imgS[4], enemyRockets[i].x, enemyRockets[i].y);
+	}
+	ctx.fillStyle = '#FFFF00';
+	ctx.font = 'bold 30pt Arial';
+	ctx.fillText(score + '', 50, 50);
+	ctx.fillText(userPositionX + '' + userPositionY, 550, 50);
+}
+
+function imagesInit() {
+	for (let i = 0; i <= 4; i++) {
+		let tmp = new Image();
+		tmp.src = imgList[i];
+		imgS.push(tmp);
+	}
 }
--- main.js
+++ main.js
@@ -5,8 +5,8 @@ import { draw } from "./js/draw.js";
 
 const imgX = [800, 77, 20, 80, 20];
 const imgY = [600, 100, 97, 80, 30];
-let imgS = [];
-let userPositionX = 100; userPositionX = 400;
+
+let userPositionX = 100, userPositionY = 400;
 let userStepX = 4, userStepY = 3;
 let userMoveX = 0, userMoveY = 0;
 let rockets = [];
